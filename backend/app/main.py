e207784a82db1ec37aaf3c20b2735e821d507c33
InnovaJeruth <innovacionjeruth2025@gmail.com>
2026-04-12 16:26:17 -0500

    Cambiando a Python 3.12 para coincidir con local

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,8 @@ app = FastAPI(title="Sistema de Tallaje SAMITEX", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    #allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
